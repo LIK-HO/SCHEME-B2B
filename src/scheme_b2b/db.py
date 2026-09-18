c95bd9dd0f75ede040a9047d523dac6fd69db3c2
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-18 05:48:50 +0300

    init: add src/scheme_b2b/db.py

---
+++ src/scheme_b2b/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .config import Settings
+from .models import Base
+
+
+def create_session_factory(settings: Settings):
+    connect_args = {"check_same_thread": False} if settings.local_db_url.startswith("sqlite") else {}
+    engine = create_engine(
+        settings.local_db_url,
+        future=True,
+        pool_pre_ping=True,
+        connect_args=connect_args,
+    )
+    Base.metadata.create_all(engine)
+    return sessionmaker(bind=engine, autoflush=False, expire_on_commit=False)
