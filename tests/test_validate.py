1320721be6728d300269744ca37f79db04fc4eeb
LIK-HO <ivanov.eugene.y@gmail.com>
2026-09-19 11:18:59 +0300

    test: expand requisites edge-case coverage

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -10,6 +10,30 @@ def test_valid_company_pair():
     assert not ogrnip
 
 
+def test_valid_ip_pair():
+    ok, reason, inn, ogrn, ogrnip = validate_requisites("540100000099", "", "304540100000002")
+    assert ok
+    assert reason == "OK"
+    assert inn == "540100000099"
+    assert not ogrn
+    assert ogrnip == "304540100000002"
+
+
 def test_reject_wrong_pair_type():
-    ok, *_ = validate_requisites("7707083893", "", "304540100000001")
+    ok, *_ = validate_requisites("7707083893", "", "304540100000002")
+    assert not ok
+
+
+def test_reject_both_registry_types():
+    ok, *_ = validate_requisites("7707083893", "1027700132195", "304540100000002")
+    assert not ok
+
+
+def test_reject_bad_inn():
+    ok, *_ = validate_requisites("7707083894", "1027700132195")
+    assert not ok
+
+
+def test_reject_bad_ogrn():
+    ok, *_ = validate_requisites("7707083893", "1027700132194")
     assert not ok
